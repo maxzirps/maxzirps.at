c1a2332326ff8509843f3cd7b17e4e9daf73d178
Maximilian Zirps <29249513+maxzirps@users.noreply.github.com>
2024-08-28 20:23:02 +0200

    implement various drafts

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # maxzirps.at
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/f2c9e34d-7de5-4e25-afb4-4206cd4e0da0/deploy-status)](https://app.netlify.com/sites/maxzirps/deploys)
-
 Personal website built with [Astro](https://astro.build/), [Solid](https://www.solidjs.com/) & [Tailwind](https://tailwindcss.com/)
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,48 @@
+// /** @type {import('tailwindcss').Config} */
+
+// const colors = require("tailwindcss/colors");
+
+// export default {
+//   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "#FAFAFA", // Very Light Gray
+//         secondary: "#FFFFFF", // White
+//         textPrimary: "#333333", // Charcoal
+//         textSecondary: "#666666", // Medium Gray
+//         accent: "#FF6F61", // Muted Coral
+//         highlight: "#FFC107", // Soft Amber
+//         border: "#E0E0E0", // Light Gray
+//         success: "#3CB371", // Medium Sea Green
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      heading: ["Cormorant Garamond", "serif", "system-ui"],
+      heading2: ["Merriweather", "serif", "system-ui"],
+    },
+    extend: {
+      colors: {
+        ...colors,
+        black: "#010417",
+        blue: "#0029c1",
+        teal: "#2bb59c",
+        yellow: "#f7dc09",
+        red: "#e22b00",
+        ...colors,
+      },
+    },
   },
   plugins: [],
 };
