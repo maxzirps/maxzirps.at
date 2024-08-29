d469057e37371e82347d029fb23d0c3bca134485
Maximilian Zirps <29249513+maxzirps@users.noreply.github.com>
2024-08-29 13:05:52 +0200

    added dark mode and further design improvements

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,32 @@
 const colors = require("tailwindcss/colors");
 
 export default {
+  darkMode: "selector",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      heading: ["Cormorant Garamond", "serif", "system-ui"],
       heading2: ["Merriweather", "serif", "system-ui"],
+      heading: ["Exo 2 Variable", "sans-serif", "system-ui"],
+    },
+    extend: {
+      keyframes: {
+        expand: {
+          "0%": {
+            width: "0",
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        shimmer: {
+          "0%": {
+            "background-position": "-500%",
+          },
+          "100%": {
+            "background-position": "500%",
+          },
+        },
+      },
     },
   },
   plugins: [],
