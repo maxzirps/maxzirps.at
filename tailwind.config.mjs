37d4a345d901d4be8d81645bfcee720820cd4c77
Maximilian Zirps <29249513+maxzirps@users.noreply.github.com>
2024-09-06 11:49:56 +0200

    remove unused code; add Sectionlayout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,6 @@
 const colors = require("tailwindcss/colors");
 
 export default {
-  darkMode: "selector",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   plugins: [],
 };
