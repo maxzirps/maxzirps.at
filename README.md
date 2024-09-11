ab116de9cc4236393695a3a5afc0c4320df41f5e
Maximilian Zirps <29249513+maxzirps@users.noreply.github.com>
2024-09-11 08:09:18 +0200

    v1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# maxzirps.at
+
+Personal website built with [Astro](https://astro.build/) & [Tailwind](https://tailwindcss.com/)
---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [],
+};
